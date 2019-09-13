63e07b11d552362381e50a944b7d95c98d1a1bcf
Ronald T. Fernandez <ronaldtfernandez@gmail.com>
2019-09-13 12:27:56 +0200

    Added custom constructors and aspects

---
+++ serverless/custom_s3.py
@@ -0,0 +1,28 @@
+import typing
+from aws_cdk import (
+    aws_s3 as s3,
+    aws_kms,
+    core
+)
+
+from aws_cdk.core import IAspect, IConstruct
+import jsii
+
+class MyBucketDefinition(s3.Bucket):
+
+  def __init__(self, scope: core.Construct, id: str, *, access_control: typing.Optional["BucketAccessControl"]=None, block_public_access: typing.Optional["BlockPublicAccess"]=None, bucket_name: typing.Optional[str]=None, cors: typing.Optional[typing.List["CorsRule"]]=None, encryption: typing.Optional["BucketEncryption"]=None, encryption_key: typing.Optional[aws_kms.IKey]=None, lifecycle_rules: typing.Optional[typing.List["LifecycleRule"]]=None, metrics: typing.Optional[typing.List["BucketMetrics"]]=None, public_read_access: typing.Optional[bool]=None, removal_policy: typing.Optional[core.RemovalPolicy]=None, versioned: typing.Optional[bool]=None, website_error_document: typing.Optional[str]=None, website_index_document: typing.Optional[str]=None, website_redirect: typing.Optional["RedirectTarget"]=None, website_routing_rules: typing.Optional[typing.List["RoutingRule"]]=None) -> None:
+    super().__init__(scope = scope, id = id, access_control = access_control, block_public_access=block_public_access, bucket_name=bucket_name, cors=cors, encryption=encryption, encryption_key= encryption_key, lifecycle_rules=lifecycle_rules, metrics=metrics, public_read_access=public_read_access, removal_policy=removal_policy, versioned=versioned, website_error_document=website_error_document, website_redirect=website_redirect, website_routing_rules=website_routing_rules)
+
+    self.node.apply_aspect(SkipBucketWithNames(bucket_name, type(self)))
+
+
+@jsii.implements(IAspect)
+class SkipBucketWithNames():
+    def __init__(self, bucket_name, type):
+        self._bucket_name = bucket_name
+        self._type = type
+    def visit(self, node: IConstruct) -> None:
+        if self._type is s3.Bucket or self._type is MyBucketDefinition:
+            has_number = any(char.isdigit() for char in self._bucket_name)
+            if has_number:
+                raise ValueError("Invalid Bucket Name <" +self._bucket_name + "> (it should not contain numbers)")
